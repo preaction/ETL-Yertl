435cc5fcd8f46e0242c002c47041186ba8614000
Doug Bell <doug.bell@baml.com>
2014-04-23 16:17:23 -0400

    initial skeleton

---
+++ lib/YAML/Filter.pm
@@ -0,0 +1,22 @@
+package YAML::Filter;
+# ABSTRACT: Filter YAML with a series of rules
+
+use strict;
+use warnings;
+
+
+
+1;
+__END__
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 ARGUMENTS
+
+=head1 OPTIONS
+
+=head1 ATTRIBUTES
+
+=head1 METHODS
