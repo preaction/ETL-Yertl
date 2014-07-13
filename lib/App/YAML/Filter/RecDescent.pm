7f425ef5ae7c98402034a4557f2a3acd2d446473
Doug Bell <madcityzen@gmail.com>
2014-07-13 00:09:16 -0500

    add list object to differentiate arrays
    Most things can return lists, but some things want to return an array.
    Since Parse::RecDescent can only return scalars, we need some way of
    differentiating between a filter or expression returning multiple
    things, and a filter or expression returning an array.
    
    A list is just a blessed array and can be flattened. When we only want
    one thing, a list will take one item, while an array is one thing.
    
    This is a lot like Perl's way of doing it, except we have to use
    arrayrefs for both circumstances.

--- lib/App/YAML/Filter/RecDescent.pm
+++ lib/App/YAML/Filter/RecDescent.pm
@@ -8,25 +8,37 @@ use Parse::RecDescent;
 $::RD_HINT = 1;
 
 my $grammar = q{
-    { sub one { return ref $_[0] eq 'ARRAY' ? $_[0]->[0] : $_[0] } }
+    {
+        sub one { return is_list( $_[0] ) ? $_[0]->[0] : $_[0] }
+        sub is_list { return ref $_[0] eq 'list' }
+        sub flatten { map { is_list( $_ ) ? @$_ : $_ } @_ }
+        sub list { return bless [ flatten( @_ ) ], 'list' }
+    }
 
     program: statement ( comb statement )(s?)
         {
             use Data::Dumper;
             yq::diag( 1, "Program: " . Dumper( \@item ) );
-            $return = $item[1];
+            $return = list( $item[1] );
         }
 
     statement: conditional | expr
+        {
+            use Data::Dumper;
+            yq::diag( 1, "Statement: " . Dumper( \@item ) );
+            $return = $item[1];
+        }
 
     conditional: 'if' expr 'then' expr ( 'else' expr )(?)
         {
-            $return = $item[2] ? $item[4] : $item[5][1];
+            use Data::Dumper;
+            yq::diag( 1, "Conditional: " . Dumper( \@item ) );
+            $return = list( one( $item[2] ) ? $item[4] : $item[5][0] );
         }
 
     expr: function_call | hash | array | binop | filter | quote_string | word
         {
-            $return = $item[1];
+            $return = list( $item[1] );
             use Data::Dumper;
             yq::diag( 1, "Expr: " . Dumper( \@item ) );
         }
@@ -36,22 +48,24 @@ my $grammar = q{
             use Data::Dumper;
             yq::diag( 1, "Filter: " . Dumper( \@item ) );
             my @keys = @{$item[3]};
-            $return = [ $::document ];
+            $return = list( $::document );
             for my $key ( @keys ) {
                 yq::diag( 1, "Key: " . Dumper( $key ) );
                 if ( $key =~ /^\[\]$/ ) {
-                    $return = $return->[0];
+                    $return = list( @{ $return->[0] } );
                 }
                 elsif ( $key =~ /^\[(\d+)\]$/ ) {
-                    $return = [ $return->[0][ $1 ] ];
+                    $return = list( $return->[0][ $1 ] );
                 }
                 elsif ( $key =~ /^\w+$/ ) {
-                    $return = [ $return->[0]{ $key } ];
+                    $return = list( $return->[0]{ $key } );
                 }
                 else {
                     die "Invalid filter key '$key'";
                 }
             }
+            # Make RD commit to this if the filter is just '.'
+            $return;
         }
 
     filter_part: word | '[' number(?) ']'
@@ -67,7 +81,7 @@ my $grammar = q{
     binop: (filter|word|quote_string) op (filter|word|quote_string)
         {
             use Data::Dumper;
-            yq::diag( 1, "Binop: " . Dumper( \@item ) );
+            yq::diag( 1, "Binop: " . Dumper( [ one($item[1]), $item[2], one($item[3]) ] ) );
             use boolean qw( true false );
             my ( $lhs_value, $cond, $rhs_value ) = ( one($item[1]), $item[2], one($item[3]) );
             # These operators suppress undef warnings, treating undef as just
@@ -102,6 +116,7 @@ my $grammar = q{
             elsif ( $cond eq '<=' ) {
                 $return = $lhs_value <= $rhs_value ? true : false;
             }
+            $return = list( $return );
         }
 
     function_call: function_name arguments(?)
@@ -114,44 +129,44 @@ my $grammar = q{
                 if ( @$args ) {
                     warn "empty does not take arguments\n";
                 }
-                $return = undef;
+                $return = list( undef );
             }
             elsif ( $func eq 'select' || $func eq 'grep' ) {
                 if ( !@$args ) {
                     warn "'$func' takes an expression argument";
-                    $return = undef;
+                    $return = list( undef );
                 }
                 else {
-                    $return = $args->[0] ? $::document : undef;
+                    $return = one( $args->[0] ) ? list( $::document ) : list( undef );
                 }
             }
             elsif ( $func eq 'group_by' ) {
-                push @{ $::scope->{ group_by }{ $args->[0] } }, $::document;
-                $return = undef;
+                push @{ $::scope->{ group_by }{ one( $args->[0] ) } }, $::document;
+                $return = list( undef );
             }
             elsif ( $func eq 'keys' ) {
-                my $value = $args->[0] || $::document;
+                my $value = @$args ? one( $args->[0] ) : $::document;
                 if ( ref $value eq 'HASH' ) {
-                    $return = [ keys %$value ];
+                    $return = list( [ keys %$value ] );
                 }
                 elsif ( ref $value eq 'ARRAY' ) {
-                    $return = [ 0..$#{ $value } ];
+                    $return = list( [ 0..$#{ $value } ] );
                 }
                 else {
                     warn "keys() requires a hash or array";
-                    $return = undef;
+                    $return = list( undef );
                 }
             }
             elsif ( $func eq 'length' ) {
-                my $value = $args->[0] || $::document;
+                my $value = @$args ? one( $args->[0] ) : $::document;
                 if ( ref $value eq 'HASH' ) {
-                    $return = keys %$value;
+                    $return = list( scalar keys %$value );
                 }
                 elsif ( ref $value eq 'ARRAY' ) {
-                    $return = @$value;
+                    $return = list( scalar @$value );
                 }
                 elsif ( !ref $value ) {
-                    $return = length $value;
+                    $return = list( length $value );
                 }
                 else {
                     warn "length() requires a hash, array, string, or number";
@@ -166,20 +181,21 @@ my $grammar = q{
             yq::diag( 1, "Hash: " . Dumper \@item );
             $return = {};
             for my $i ( @{$item[2]} ) {
-                $return->{ $i->[0] } = $i->[1];
+                $return->{ one( $i->[0] ) } = one( $i->[1] );
             }
+            $return = list( $return );
         }
 
     array: '[' expr(s /,/) ']'
         {
             use Data::Dumper;
             yq::diag( 1, "Array: " . Dumper( \@item ) );
-            $return = $item[1];
+            $return = [ flatten( @{ $item[2] } ) ];
         }
 
     arguments: '(' expr(s /,/) ')'
         {
-            $return = $item[2];
+            $return = list( @{ $item[2] } );
             use Data::Dumper;
             yq::diag( 1, "Args: " . Dumper( \@item ) );
         }
@@ -209,19 +225,24 @@ my $grammar = q{
 
 my $parser = Parse::RecDescent->new( $grammar );
 
+sub is_list {
+    return ref $_[0] eq 'list';
+}
+
 sub filter {
     my ( $class, $filter, $doc, $scope ) = @_;
     $yq::VERBOSE = 1;
     $::document = $doc;
     $::scope = $scope;
     my $output = $parser->program( $filter );
-    #; use Data::Dumper;
-    #; print "Want array: " . wantarray;
-    #; print "OUTPUT: " . Dumper $output;
-    if ( wantarray && ref $output eq 'ARRAY' ) {
+    ; use Data::Dumper;
+    ; print "Want array: " . wantarray;
+    ; print "OUTPUT: " . Dumper $output;
+    ; print "SCOPE: " . Dumper $scope;
+    if ( wantarray && is_list( $output ) ) {
         return @$output;
     }
-    elsif ( ref $output eq 'ARRAY' ) {
+    elsif ( is_list( $output ) ) {
         return $output->[-1];
     }
     else {
