69f1204ad6054ad5d264367cc8a944f0791ed98d
Doug Bell <madcityzen@gmail.com>
2014-12-24 06:11:13 -0600

    add csv format module
    This one does not do very well: It treats CSV as line-delimited, which
    is completely wrong.
    
    We need to do some kind of ETL::Yertle::Stream object which can wrap
    strings and filehandles to do things, and pass those into formatters...

---
+++ lib/ETL/Yertl/Format/csv.pm
@@ -0,0 +1,197 @@
+package ETL::Yertl::Format::csv;
+# ABSTRACT: CSV read/write support for Yertl
+
+use ETL::Yertl 'Class';
+use Module::Runtime qw( use_module );
+use List::Util qw( pairs pairkeys pairfirst );
+use Text::Trim qw( ltrim );
+
+=attr trim
+
+If true, trim off leading whitespace from the cells. Defaults to true.
+
+Some CSV documents are formatted to line up the commas for easy visual
+scanning. This removes that whitespace.
+
+=cut
+
+has trim => (
+    is => 'ro',
+    isa => Bool,
+    default => sub { 1 },
+);
+
+=attr format_module
+
+The module being used for this format. Possible modules, in order of importance:
+
+=over 4
+
+=item L<Text::CSV_XS> (any version)
+
+=item L<Text::CSV> (any version)
+
+=back
+
+=cut
+
+# Pairs of module => supported version
+our @FORMAT_MODULES = (
+    'Text::CSV_XS' => 0,
+    'Text::CSV' => 0,
+);
+
+has format_module => (
+    is => 'rw',
+    isa => sub {
+        my ( $format_module ) = @_;
+        die "format_module must be one of: " . join " ", pairkeys @FORMAT_MODULES
+            unless pairfirst { $a eq $format_module } @FORMAT_MODULES;
+        eval {
+            use_module( $format_module );
+        };
+        if ( $@ ) {
+            die "Could not load format module '$format_module'";
+        }
+    },
+    lazy => 1,
+    default => sub {
+        for my $format_module ( pairs @FORMAT_MODULES ) {
+            eval {
+                use_module( @$format_module );
+            };
+            if ( !$@ ) {
+                return $format_module->[0];
+            }
+        }
+        die "Could not load a formatter for CSV. Please install one of the following modules:\n"
+            . join "",
+                map { sprintf "\t%s (%s)", $_->[0], $_->[1] ? "version $_->[1]" : "Any version" }
+                pairs @FORMAT_MODULES;
+    },
+);
+
+
+# Hash of MODULE => formatter sub
+my %FORMAT_SUB = (
+
+    'Text::CSV_XS' => {
+        to => sub {
+            my $self = shift;
+            state $csv = Text::CSV_XS->new;
+            state @names;
+            my $str;
+
+            if ( !@names ) {
+                @names = sort keys %{ $_[0] };
+                $csv->combine( @names );
+                $str .= $csv->string . $/;
+            }
+
+            for my $doc ( @_ ) {
+                $csv->combine( map { $doc->{ $_ } } @names );
+                $str .= $csv->string . $/;
+            }
+
+            return $str;
+        },
+
+        from => sub {
+            my $self = shift;
+            state $csv = Text::CSV_XS->new;
+            state @names;
+
+            if ( !@names ) {
+                $csv->parse( shift );
+                @names = $csv->fields;
+            }
+
+            my @docs;
+            for my $line ( @_ ) {
+                $csv->parse( $line );
+                my @values = $csv->fields;
+                my $doc = { map {; $names[ $_ ] => $values[ $_ ] } 0..$#values };
+
+                if ( $self->trim ) {
+                    ltrim for values %$doc;
+                }
+
+                push @docs, $doc;
+            }
+
+            return @docs;
+        },
+    },
+
+    'Text::CSV' => {
+        to => sub {
+            my $self = shift;
+            state $csv = Text::CSV->new;
+            state @names;
+            my $str;
+
+            if ( !@names ) {
+                @names = sort keys %{ $_[0] };
+                $csv->combine( @names );
+                $str .= $csv->string . $/;
+            }
+
+            for my $doc ( @_ ) {
+                $csv->combine( map { $doc->{ $_ } } @names );
+                $str .= $csv->string . $/;
+            }
+
+            return $str;
+        },
+
+        from => sub {
+            my $self = shift;
+            state $csv = Text::CSV->new;
+            state @names;
+
+            if ( !@names ) {
+                $csv->parse( shift );
+                @names = $csv->fields;
+            }
+
+            my @docs;
+            for my $line ( @_ ) {
+                $csv->parse( $line );
+                my @values = $csv->fields;
+                my $doc = { map {; $names[ $_ ] => $values[ $_ ] } 0..$#values };
+
+                if ( $self->trim ) {
+                    ltrim for values %$doc;
+                }
+
+                push @docs, $doc;
+            }
+
+            return @docs;
+        },
+
+    },
+);
+
+=method to( DOCUMENTS )
+
+Convert the given C<DOCUMENTS> to CSV. Returns a CSV string.
+
+=cut
+
+sub to {
+    my $self = shift;
+    return $FORMAT_SUB{ $self->format_module }{to}->( $self, @_ );
+}
+
+=method from( CSV )
+
+Convert the given C<CSV> string into documents. Returns the list of values extracted.
+
+=cut
+
+sub from {
+    my $self = shift;
+    return $FORMAT_SUB{ $self->format_module }{from}->( $self, @_ );
+}
+
---
+++ t/format/csv.t
@@ -0,0 +1,63 @@
+
+use ETL::Yertl 'Test';
+use Test::Lib;
+use ETL::Yertl::Format::csv;
+
+my $CLASS = 'ETL::Yertl::Format::csv';
+
+my $EXPECT_TO = <<ENDJSON;
+bar,baz,foo
+2,3,one
+4,5,two
+ENDJSON
+
+my @EXPECT_FROM = (
+    {
+        bar => 2,
+        baz => 3,
+        foo => 'one',
+    },
+    {
+        bar => 4,
+        baz => 5,
+        foo => 'two',
+    },
+);
+
+subtest 'constructor' => sub {
+    subtest 'invalid format module' => sub {
+        throws_ok {
+            $CLASS->new( format_module => 'Not::Supported' );
+        } qr{format_module must be one of: Text::CSV_XS Text::CSV};
+    };
+};
+
+subtest 'Text::CSV_XS' => sub {
+    my $formatter = $CLASS->new( format_module => 'Text::CSV_XS' );
+    eq_or_diff $formatter->to( @EXPECT_FROM ), $EXPECT_TO;
+    my $got = [ $formatter->from( split /\n/, $EXPECT_TO ) ];
+    cmp_deeply $got, \@EXPECT_FROM or diag explain $got;
+};
+
+subtest 'Text::CSV' => sub {
+    my $formatter = $CLASS->new( format_module => 'Text::CSV' );
+    eq_or_diff $formatter->to( @EXPECT_FROM ), $EXPECT_TO;
+    my $got = [ $formatter->from( split /\n/, $EXPECT_TO ) ];
+    cmp_deeply $got, \@EXPECT_FROM or diag explain $got;
+};
+
+subtest 'no formatter available' => sub {
+    local @ETL::Yertl::Format::csv::FORMAT_MODULES = (
+        'Not::CSV::Module' => 0,
+        'Not::Other::Module' => 0,
+        'LowVersion' => 1,
+    );
+    throws_ok {
+        $CLASS->new->format_module;
+    } qr{Could not load a formatter for CSV[.] Please install one of the following modules:};
+    like $@, qr{Not::CSV::Module \(Any version\)};
+    like $@, qr{Not::Other::Module \(Any version\)};
+    like $@, qr{LowVersion \(version 1\)};
+};
+
+done_testing;
