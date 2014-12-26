78bcc91c7ec9c952466abc3416326a77a3758975
Doug Bell <madcityzen@gmail.com>
2014-12-26 01:10:35 -0600

    add yaml formatter class
    This is the default format for message-passing between commands on the
    shell

---
+++ lib/ETL/Yertl/Format/yaml.pm
@@ -0,0 +1,130 @@
+package ETL::Yertl::Format::yaml;
+# ABSTRACT: YAML read/write support for Yertl
+
+use ETL::Yertl 'Class';
+use Module::Runtime qw( use_module );
+use List::Util qw( pairs pairkeys pairfirst );
+
+=attr format_module
+
+The module being used for this format. Possible modules, in order of importance:
+
+=over 4
+
+=item L<YAML::XS> (any version)
+
+=item L<YAML::Syck> (any version)
+
+=item L<YAML> (any version)
+
+=back
+
+=cut
+
+# Pairs of module => supported version
+our @FORMAT_MODULES = (
+    'YAML::XS' => 0,
+    'YAML::Syck' => 0,
+    'YAML' => 0,
+);
+
+has format_module => (
+    is => 'rw',
+    isa => sub {
+        my ( $format_module ) = @_;
+        die "format_module must be one of: " . join " ", pairkeys @FORMAT_MODULES
+            unless pairfirst { $a eq $format_module } @FORMAT_MODULES;
+        eval {
+            use_module( $format_module );
+        };
+        if ( $@ ) {
+            die "Could not load format module '$format_module'";
+        }
+    },
+    lazy => 1,
+    default => sub {
+        for my $format_module ( pairs @FORMAT_MODULES ) {
+            eval {
+                use_module( @$format_module );
+            };
+            if ( !$@ ) {
+                return $format_module->[0];
+            }
+        }
+        die "Could not load a formatter for YAML. Please install one of the following modules:\n"
+            . join "",
+                map { sprintf "\t%s (%s)", $_->[0], $_->[1] ? "version $_->[1]" : "Any version" }
+                pairs @FORMAT_MODULES;
+    },
+);
+
+
+# Hash of MODULE => formatter sub
+my %FORMAT_SUB = (
+
+    'YAML::XS' => {
+        to => sub {
+            my $self = shift;
+            return YAML::XS::Dump( @_ );
+        },
+
+        from => sub {
+            my $self = shift;
+            return YAML::XS::Load( @_ );
+        },
+
+    },
+
+    'YAML::Syck' => {
+        to => sub {
+            my $self = shift;
+            return YAML::Syck::Dump( @_ );
+        },
+
+        from => sub {
+            my $self = shift;
+            return YAML::Syck::Load( @_ );
+        },
+
+    },
+
+    'YAML' => {
+        to => sub {
+            my $self = shift;
+            return YAML::Dump( @_ );
+        },
+
+        from => sub {
+            my $self = shift;
+            return YAML::Load( @_ );
+        },
+
+    },
+);
+
+=method to( DOCUMENTS )
+
+Convert the given C<DOCUMENTS> to YAML. Returns a YAML string.
+
+=cut
+
+sub to {
+    my $self = shift;
+    return $FORMAT_SUB{ $self->format_module }{to}->( $self, @_ );
+}
+
+=method from( YAML )
+
+Convert the given C<YAML> string into documents. Returns the list of values extracted.
+
+=cut
+
+sub from {
+    my $self = shift;
+    return $FORMAT_SUB{ $self->format_module }{from}->( $self, @_ );
+}
+
+1;
+__END__
+
+
---
+++ t/format/yaml.t
@@ -0,0 +1,71 @@
+
+use ETL::Yertl 'Test';
+use Test::Lib;
+use YAML;
+use ETL::Yertl::Format::yaml;
+
+my $CLASS = 'ETL::Yertl::Format::yaml';
+
+my $EXPECT_TO = <<ENDYAML;
+---
+baz: buzz
+foo: bar
+---
+flip:
+  - flop
+  - blip
+---
+- foo
+- bar
+- baz
+ENDYAML
+
+my @EXPECT_FROM = (
+    {
+        foo => 'bar',
+        baz => 'buzz',
+    },
+    {
+        flip => [qw( flop blip )],
+    },
+    [qw( foo bar baz )],
+);
+
+subtest 'constructor' => sub {
+    subtest 'invalid format module' => sub {
+        throws_ok {
+            $CLASS->new( format_module => 'Not::Supported' );
+        } qr{format_module must be one of: YAML::XS YAML::Syck YAML};
+    };
+};
+
+for my $format_module ( qw( YAML::XS YAML::Syck YAML ) ) {
+    subtest $format_module => sub {
+        my $formatter = $CLASS->new( format_module => $format_module );
+        # Can't compare against strings because whitespace is both significant
+        # and differs between implementations
+        # And YAML::XS and YAML are _incompatible_. See: https://github.com/ingydotnet/yaml-libyaml-pm/issues/9
+        my $got_yaml = $formatter->to( @EXPECT_FROM );
+        no strict 'refs';
+        cmp_deeply [ "${format_module}::Load"->( $got_yaml ) ], \@EXPECT_FROM or diag $got_yaml;
+
+        my $got = [ $formatter->from( $EXPECT_TO ) ];
+        cmp_deeply $got, \@EXPECT_FROM or diag explain $got;
+    };
+}
+
+subtest 'no formatter available' => sub {
+    local @ETL::Yertl::Format::yaml::FORMAT_MODULES = (
+        'Not::YAML::Module' => 0,
+        'Not::Other::Module' => 0,
+        'LowVersion' => 1,
+    );
+    throws_ok {
+        $CLASS->new->format_module;
+    } qr{Could not load a formatter for YAML[.] Please install one of the following modules:};
+    like $@, qr{Not::YAML::Module \(Any version\)};
+    like $@, qr{Not::Other::Module \(Any version\)};
+    like $@, qr{LowVersion \(version 1\)};
+};
+
+done_testing;
