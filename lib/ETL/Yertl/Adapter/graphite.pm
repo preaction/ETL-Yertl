44a7fdd80e16910c998198e763ae424d181b6c4f
Doug Bell <doug@preaction.me>
2017-09-22 00:29:49 -0500

    add graphite time series adapter

---
+++ lib/ETL/Yertl/Adapter/graphite.pm
@@ -0,0 +1,253 @@
+package ETL::Yertl::Adapter::graphite;
+our $VERSION = '0.033';
+# ABSTRACT: Adapter to read/write from Graphite time series database
+
+=head1 SYNOPSIS
+
+    my $db = ETL::Yertl::Adapter::graphite->new( 'graphite://localhost:8086' );
+    my @points = $db->read_ts( { metric => 'db.cpu_load.1m' } );
+    $db->write_ts( { metric => 'db.cpu_load.1m', value => 1.23 } );
+
+=head1 DESCRIPTION
+
+This class allows Yertl to read and write time series from L<the
+Graphite time series system|https://graphiteapp.org>. Yertl can write to
+Carbon using the "plaintext" protocol, and read from Graphite using its
+HTTP API.
+
+This adapter is used by the L<yts> command.
+
+=head2 Metric Name Format
+
+Graphite metrics are paths separated by C<.>, which is the native format
+that Yertl supports.
+
+    yts graphite://localhost foo.bar.baz
+    yts graphite://localhost foo.bar
+
+=head1 SEE ALSO
+
+L<ETL::Yertl>, L<yts>,
+L<Write to Carbon|http://graphite.readthedocs.io/en/latest/feeding-carbon.html>
+L<Read from Graphite|http://graphite.readthedocs.io/en/latest/render_api.html>
+
+=cut
+
+use ETL::Yertl 'Class';
+use Net::Async::HTTP;
+use URI;
+use JSON::MaybeXS qw( decode_json );
+use List::Util qw( first );
+use DateTime::Format::ISO8601;
+use IO::Async::Loop;
+
+has host => ( is => 'ro', required => 1 );
+has write_port => ( is => 'ro', default => 2003 ); # "plaintext" port
+has http_port => ( is => 'ro', default => 8080 ); # http port
+
+has _loop => (
+    is => 'ro',
+    default => sub {
+        IO::Async::Loop->new();
+    },
+);
+
+has write_client => (
+    is => 'ro',
+    lazy => 1,
+    default => sub {
+        my ( $self ) = @_;
+        return $self->_loop->connect(
+            socktype => 'stream',
+            host => $self->host,
+            service => $self->write_port,
+        )->get;
+    },
+);
+
+has http_client => (
+    is => 'ro',
+    lazy => 1,
+    default => sub {
+        my ( $self ) = @_;
+        my $http = Net::Async::HTTP->new;
+        $self->_loop->add( $http );
+        return $http;
+    },
+);
+
+has dt_fmt => (
+    is => 'ro',
+    lazy => 1,
+    default => sub {
+        DateTime::Format::ISO8601->new;
+    },
+);
+
+=method new
+
+    my $db = ETL::Yertl::Adapter::graphite->new( 'graphite://localhost' );
+    my $db = ETL::Yertl::Adapter::graphite->new( 'graphite://localhost:8080' );
+
+Construct a new Graphite adapter for the database on the given host and port.
+Port is optional and defaults to C<2003> (the Carbon plaintext port) for writing
+and C<8080> (the default Graphite HTTP API port) for reading.
+
+=cut
+
+sub BUILDARGS {
+    my ( $class, @args ) = @_;
+    my %args;
+    if ( @args == 1 ) {
+        if ( $args[0] =~ m{://([^:]+)(?::([^/]+))?} ) {
+            ($args{host}, my $port ) = ( $1, $2 );
+            if ( $port ) {
+                @args{qw( http_port write_port )} = ( $port ) x 2;
+            }
+        }
+    }
+    else {
+        %args = @args;
+    }
+    return \%args;
+}
+
+=method read_ts
+
+    my @points = $db->read_ts( $query );
+
+Read a time series from the database. C<$query> is a hash reference
+with the following keys:
+
+=over
+
+=item metric
+
+The time series to read. For Graphite, this is a  path separated by dots
+(C<.>).
+
+=item start
+
+An ISO8601 date/time for the start of the series points to return,
+inclusive.
+
+=item end
+
+An ISO8601 date/time for the end of the series points to return,
+inclusive.
+
+=item tags
+
+B<NOTE>: Graphite does not support per-value tags. Using this field will
+cause a fatal error.
+
+=back
+
+=cut
+
+sub read_ts {
+    my ( $self, $query ) = @_;
+    die "Tags are not supported by Graphite" if $query->{tags} && keys %{ $query->{tags} };
+    my $metric = $query->{ metric };
+
+    my %form = (
+        target => $metric,
+        format => 'json',
+    );
+
+    if ( $query->{ start } ) {
+        $form{ from } = _format_graphite_dt( $query->{start} );
+    }
+    if ( $query->{ end } ) {
+        $form{ until } = _format_graphite_dt( $query->{end} );
+    }
+
+    my $url = URI->new( sprintf 'http://%s:%s/render', $self->host, $self->http_port );
+    $url->query_form( %form );
+
+    #; say "Fetching $url";
+    my $res = $self->http_client->GET( $url )->get;
+
+    #; say $res->decoded_content;
+    if ( $res->is_error ) {
+        die sprintf "Error fetching metric '%s': " . $res->decoded_content . "\n", $metric;
+    }
+
+    my $result = decode_json( $res->decoded_content );
+    my $fmt = $self->dt_fmt;
+    my @points;
+    for my $series ( @{ $result } ) {
+        for my $point ( @{ $series->{datapoints} } ) {
+            push @points, {
+                metric => $series->{target},
+                timestamp => DateTime->from_epoch( epoch => $point->[1] )->iso8601 . 'Z',
+                value => $point->[0],
+            };
+        }
+    }
+
+    return @points;
+}
+
+=method write_ts
+
+    $db->write_ts( @points );
+
+Write time series points to the database. C<@points> is an array
+of hashrefs with the following keys:
+
+=over
+
+=item metric
+
+The metric to write. For Graphite, this is a path separated by dots
+(C<.>).
+
+=item timestamp
+
+An ISO8601 timestamp. Optional. Defaults to the current time on the
+InfluxDB server.
+
+=item value
+
+The metric value.
+
+=item tags
+
+B<NOTE>: Graphite does not support per-value tags. Using this field will
+cause a fatal error.
+
+=back
+
+=cut
+
+sub write_ts {
+    my ( $self, @points ) = @_;
+    my $sock = $self->write_client;
+    for my $point ( @points ) {
+        die "Tags are not supported by Graphite" if $point->{tags} && keys %{ $point->{tags} };
+        $sock->write(
+            join( " ", $point->{metric}, $point->{value},
+            $self->dt_fmt->parse_datetime( $point->{timestamp} )->epoch, )
+            . "\n",
+        );
+    }
+    return;
+}
+
+#=sub _format_graphite_dt
+#
+#   my $graphite_dt = _format_graphite_dt( $iso_dt );
+#
+# Graphite supports two date/time formats: YYYYMMDD and, bizarrely,
+# HH:MM_YYYYMMDD
+sub _format_graphite_dt {
+    my ( $iso ) = @_;
+    if ( $iso =~ /^(\d{4})-?(\d{2})-?(\d{2})$/ ) {
+        return join "", $1, $2, $3;
+    }
+    $iso =~ /^(\d{4})-?(\d{2})-?(\d{2})[T ]?(\d{2}):?(\d{2})/;
+    return "$4:$5_$1$2$3";
+}
+
+1;
---
+++ t/adapter/graphite.t
@@ -0,0 +1,149 @@
+
+=head1 DESCRIPTION
+
+This test ensures that the Graphite adapter (L<ETL::Yertl::Adapter::graphite>)
+is able to read/write time series data
+
+=head1 SEE ALSO
+
+L<yts>
+
+=cut
+
+use ETL::Yertl 'Test';
+use IO::Async::Loop;
+use IO::Async::Test qw( wait_for testing_loop );
+use Mock::MonkeyPatch;
+use Future;
+use HTTP::Response;
+use JSON::PP qw( encode_json );
+use ETL::Yertl::Adapter::graphite;
+
+my $loop = IO::Async::Loop->new();
+testing_loop( $loop );
+
+subtest 'constructor spec' => sub {
+    subtest 'success' => sub {
+        my $db = ETL::Yertl::Adapter::graphite->new( 'graphite://localhost:2003' );
+        is $db->host, 'localhost', 'host is correct';
+        is $db->write_port, 2003, 'write_port is correct';
+        is $db->http_port, 2003, 'http_port is correct';
+    };
+
+    subtest 'port defaults to 8086' => sub {
+        my $db = ETL::Yertl::Adapter::graphite->new( 'graphite://localhost' );
+        is $db->host, 'localhost', 'host is correct';
+        is $db->write_port, 2003, 'write_port is correct';
+        is $db->http_port, 8080, 'http_port is correct';
+    };
+
+    subtest 'host is required' => sub {
+        dies_ok { ETL::Yertl::Adapter::graphite->new( 'graphite://:8086' ) };
+    };
+
+};
+
+subtest 'read ts' => sub {
+    my $db = ETL::Yertl::Adapter::graphite->new(
+        _loop => $loop,
+        host => 'localhost',
+    );
+
+    my $content = encode_json(
+        [{
+          target => "cpu_load.1m",
+          datapoints => [
+            [1.0, 1311836008],
+            [2.0, 1311836009],
+            [3.0, 1311836010],
+          ]
+        }]
+    );
+    my $mock = Mock::MonkeyPatch->patch(
+        'Net::Async::HTTP::GET' => sub {
+            return Future->done(
+                HTTP::Response->new(
+                    200 => 'OK',
+                    [
+                        'Content-Length' => length $content,
+                        'Content-Type' => 'text/plain',
+                    ],
+                    $content,
+                ),
+            );
+        },
+    );
+
+    my @points = $db->read_ts( { metric => 'cpu_load.1m' } );
+    cmp_deeply \@points, [
+        {
+            timestamp => '2011-07-28T06:53:28Z',
+            metric => 'cpu_load.1m',
+            value => 1.0,
+        },
+        {
+            timestamp => '2011-07-28T06:53:29Z',
+            metric => 'cpu_load.1m',
+            value => 2.0,
+        },
+        {
+            timestamp => '2011-07-28T06:53:30Z',
+            metric => 'cpu_load.1m',
+            value => 3.0,
+        },
+    ];
+
+    ok $mock->called, 'mock GET called';
+    my $args = $mock->method_arguments;
+    my $url = URI->new( $args->[0] );
+    is $url->host_port, 'localhost:8080', 'host/port is correct';
+    cmp_deeply { $url->query_form }, {
+        target => 'cpu_load.1m',
+        format => 'json',
+    }, 'query params correct';
+};
+
+subtest 'write ts' => sub {
+    my $empty = 0;
+    my ( $reader, $writer ) = IO::Async::OS->pipepair;
+    $_->blocking( 0 ) for $reader, $writer;
+
+    my $stream = IO::Async::Stream->new(
+        write_handle => $writer,
+        on_outgoing_empty => sub { $empty++ },
+    );
+    $loop->add( $stream );
+
+    my $db = ETL::Yertl::Adapter::graphite->new(
+        _loop => $loop,
+        host => 'localhost',
+        write_client => $stream,
+    );
+
+    my @points = (
+        {
+            timestamp => '2017-01-01T00:00:00.000000000Z',
+            metric => 'mydb.cpu_load.5m',
+            value => 1.23,
+        },
+        {
+            timestamp => '2017-01-01T00:05:00.000000000Z',
+            metric => 'mydb.cpu_load.1m',
+            value => 1.26,
+        },
+    );
+
+    $db->write_ts( @points );
+    wait_for { $empty };
+
+    my @lines = (
+        "mydb.cpu_load.5m 1.23 1483228800",
+        "mydb.cpu_load.1m 1.26 1483229100",
+        ""
+    );
+    my $buffer;
+    $reader->sysread( $buffer, 8192 );
+    is $buffer, join( "\n", @lines ), 'graphite plaintext protocol points correct';
+};
+
+done_testing;
